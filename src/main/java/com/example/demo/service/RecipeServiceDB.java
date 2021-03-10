d5e403482d23c6f8b3e170b16239ddb8597832de
Jasmine Dhaliwal <jasminedhaliwal29@gmail.com>
2021-03-10 16:22:00 +0000

    Created the RecipeServiceDB

---
+++ src/main/java/com/example/demo/service/RecipeServiceDB.java
@@ -0,0 +1,29 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import com.example.demo.domain.Recipe;
+
+public class RecipeServiceDB implements RecipeService {
+
+	@Override
+	public Recipe createRecipe(Recipe recipe) {
+		return null;
+	}
+
+	@Override
+	public List<Recipe> getRecipe() {
+		return null;
+	}
+
+	@Override
+	public Recipe getRecipeById(long id) {
+		return null;
+	}
+
+	@Override
+	public Recipe removeRecipe(long id) {
+		return null;
+	}
+
+}
